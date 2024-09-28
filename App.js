45b150e427ff9e8716d40d0102cb9d6da612bed4
Karolina Diaz <karolinadiaz48@gmail.com>
2024-09-27 20:51:25 -0600

    Estructura de componentes

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import 'react-native-gesture-handler';
 import { AuthProvider } from './context/AuthProvider';
 import { RootNavigator } from './stack/Stack';
 
+import Aplication from './src/pages/farmacia/Lista-Farmacia'
+
 export default function App() {
-  return (
+  return (/*
     <AuthProvider>
       <RootNavigator />
-    </AuthProvider>
+    </AuthProvider>*/
+    <Aplication/>
   );
 }
 
